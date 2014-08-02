2a0861d3a30d3bd2e145ec753735a835ab95622c
Ken <nyhunter77@yahoo.com>
2014-08-02 06:41:30 -0400

    some morning code before work ...
    starting part 2 - animate!

--- js/app.js
+++ js/app.js
@@ -3,10 +3,24 @@ $(document).ready(function() {
         console.log("mouse entered .ryu div");
         $(".ryu-still").hide();
         $(".ryu-ready").show();
-    });
-    $(".ryu").mouseleave(function() {
+    })
+    .mouseleave(function() {
         console.log("mouse left");
         $(".ryu-still").show();
         $(".ryu-ready").hide();
-    });
+    })
+    .mousedown(function() {
+        console.log("mousedown");
+        $(".ryu-ready").hide();
+        $(".ryu-throwing").show();
+        $(".hadouken").show();
+        //play sound
+        //animate fireball to right
+        })
+    .mouseup(function() {
+        console.log("mouseup");
+        $(".ryu-throwing").hide();
+        $(".ryu-ready").show();
+        //ryu goes back to ready
+        });
 });
